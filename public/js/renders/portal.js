b0b8c827bf2b7cf35d8e14ee9d59dccf27abdf78
Einar Longva <einar.longva24@gmail.com>
2020-12-22 15:10:28 -0600

    portal

---
+++ public/js/renders/portal.js
@@ -0,0 +1,9 @@
+function renderNoProducts() {
+  return (`
+    <div class='row'>
+        <div class='col'>
+            <p> There are no products available <p>
+        </div>
+    </div>
+  `);
+}
