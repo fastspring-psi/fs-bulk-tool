83fe25e08a8d49083a0c596b8e79a210d905489b
Einar Longva <einar.longva24@gmail.com>
2020-12-23 15:01:54 -0600

    portal

--- public/js/renders/portal.js
+++ public/js/renders/portal.js
@@ -25,7 +25,7 @@ function renderProductsTable(products) {
         <tr class='product-row'>
             <td> ${product.display.en} </td>
             <td> ${product.pricing.price.USD} </td>
-            <td> ${product.pricing.quantityDiscounts[1] || ''} </td>
+            <td> ${product.pricing.quantityDiscounts.1 || ''} </td>
         </tr>
   `);
 
