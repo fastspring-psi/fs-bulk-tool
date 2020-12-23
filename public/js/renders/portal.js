b44d66678529f4e7bd78d8f094dbaa2185e364ad
Einar Longva <einar.longva24@gmail.com>
2020-12-23 11:18:02 -0600

    portal

--- public/js/renders/portal.js
+++ public/js/renders/portal.js
@@ -19,7 +19,7 @@ function renderProductsTable(products) {
   const productRows = products.map((product) => `
         <tr class='product-row'>
             <td> ${product.display.en} </td>
-            <td> ${porduct.pricing.price.USD} </td>
+            <td> ${product.pricing.price.USD} </td>
             <td> ${product.pricing.quantityDiscounts || ''} </td>
             <td> ${product.dateLimits.start || ''} </td>
             <td> ${product.dateLimits.end || ''} </td>
