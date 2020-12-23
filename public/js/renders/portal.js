8a4ccf392c915a923ceeb8674dbc5fe45c31970d
Einar Longva <einar.longva24@gmail.com>
2020-12-23 11:08:47 -0600

    portal

--- public/js/renders/portal.js
+++ public/js/renders/portal.js
@@ -7,3 +7,43 @@ function renderNoProducts() {
     </div>
   `);
 }
+
+
+//TO DO:
+//Handle multiple prices
+//Need to check if there are discounts or not
+//Add logic to display discounts in a readable way
+//Decide what information to show --> what is the best way to display product information and allow for quick editing?
+//
+function renderProductsTable(products) {
+  const productRows = product.map((product) => `
+        <tr class='product-row'>
+            <td> ${product.display.en} </td>
+            <td> ${porduct.pricing.price.USD} </td>
+            <td> ${product.pricing.quantityDiscounts || ''} </td>
+            <td> ${product.dateLimits.start || ''} </td>
+            <td> ${product.dateLimits.end || ''} </td>
+        </tr>
+  `);
+
+  return (`
+      <div id="products-table-container" class="row">
+        <div class='col'>
+          <table class="table table-hover">
+            <thead>
+              <tr class="bg-info">
+                <th scope="col"> Display Name </th>
+                <th scope="col"> Price </th>
+                <th scope="col"> Discount </th>
+                <th scope="col"> Discount Start </th>
+                <th scope="col"> Discount End </th>
+              </tr>
+            </thead>
+            <tbody>
+              ${productRows.join('')}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    `);
+}
