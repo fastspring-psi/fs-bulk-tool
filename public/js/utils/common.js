fa4854b6ad890612ffa5c8c4f75653be9c3a52dd
Einar Longva <einar.longva24@gmail.com>
2021-01-06 11:29:08 -0600

    portal

--- public/js/utils/common.js
+++ public/js/utils/common.js
@@ -33,6 +33,26 @@ function submitSelectedDiscount() {
   const dateEnd = document.getElementById("dateEnd").value;
   const discReason = document.getElementById("discReason").value;
 
+  const productsTable = document.getElementById("table-products");
+  const checkBoxes = productsTable.getElementsByTagName("INPUT");
+  const productPaths = [];
+  for(var i = 0; i < checkBoxes.length; i++){
+    if(checkBoxes[i].checked){
+      var row = checkBoxes[i].parentNode.parentNode;
+      productPaths.push(row.cells[1].getAttribute("data-path"));
+    }
+  }
+
+  if(!(percent && prodQty && dateStart && dateEnd && discReason)) {
+    alert('Please fill in all discount fields');
+    return;
+  }
+
+  if(productPaths.length < 0) {
+    alert('Please select products to apply discount to');
+    return;
+  }
+
   const pricing = {
       quantityDiscounts: {},
       dateLimitsEnabled: true,
@@ -44,25 +64,11 @@ function submitSelectedDiscount() {
     products: []
   };
 
-  //// TODO:
-  // validation
   pricing.quantityDiscounts[prodQty] = percent;
   pricing.dateLimits["start"] = dateStart;
   pricing.dateLimits["end"] = dateEnd;
   pricing.discountReason["en"] = discReason;
-
-  var productsTable = document.getElementById("table-products");
-  var checkBoxes = productsTable.getElementsByTagName("INPUT");
-  var productPaths = [];
-  for(var i = 0; i < checkBoxes.length; i++){
-    if(checkBoxes[i].checked){
-      var row = checkBoxes[i].parentNode.parentNode;
-      productPaths.push(row.cells[1].getAttribute("data-path"));
-    }
-  }
-
   for(i = 0; i < productPaths.length; i++) {
-    console.log(productPaths[i]);
     payload.products[i] = {
       product: productPaths[i],
       pricing
