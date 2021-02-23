766cc363ea3717b06e843f71a7f20d6c8f13f12f
Einar Longva <einar.longva24@gmail.com>
2021-02-23 11:13:42 -0600

    additions

--- public/js/renders/forms.js
+++ public/js/renders/forms.js
@@ -23,7 +23,7 @@ function renderSelectedDiscountForm() {
             <p class="add_button">&oplus;</p>
         </div>
          <div class="req-info">
-              <span class="form-tooltip" data-toggle="tooltip" data-placement="right" data-html="true" title="<h6>Product Discount<br></h6><p>Product discount applies the given percentage to the selected quantity and more. If you would like to apply a given percentage to all products select &quot;1 or more&quot;.</p?">&nbsp;&#9432<span>
+              <span id="discount-amount-tooltip" class="form-tooltip" data-toggle="tooltip" data-placement="right" data-html="true" title="<h6>Product Discount<br></h6><p>Product discount applies the given percentage to the selected quantity and more. If you would like to apply a given percentage to all products select &quot;1 or more&quot;.</p?">&nbsp;&#9432<span>
         </div>
       </div>
       <div class="form-group" id="discount-date-range-group">
@@ -31,7 +31,7 @@ function renderSelectedDiscountForm() {
         <input class="form-control" id="dateStart" name="date" placeholder="YYYY-MM-DD" type="text" readonly/>
         <span class="input-group-text" for="dateEnd">To</span>
         <input class="form-control" id="dateEnd" name="date" placeholder="YYYY-MM-DD" type="text" readonly/>
-        <span class="form-tooltip"data-toggle="tooltip" data-placement="right" data-html="true" title="<h6>Discount Date Range<br></h6><p>The discount date range determines when your discount will start and end. Dates start at 00:00 of the given day. eg. If you would like a discount to end on YYYY-01-20 you would select YYYY-01-21 as the end date.</p>">&nbsp;&#9432<span>
+        <span id="discount-date-tooltip" class="form-tooltip"data-toggle="tooltip" data-placement="right" data-html="true" title="<h6>Discount Date Range<br></h6><p>The discount date range determines when your discount will start and end. Dates start at 00:00 of the given day. eg. If you would like a discount to end on YYYY-01-20 you would select YYYY-01-21 as the end date.</p>">&nbsp;&#9432<span>
       </div>
       <div class="form-group">
         <label class="input-group-text" for="discReason">
